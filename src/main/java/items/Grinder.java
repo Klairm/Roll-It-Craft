f517f0d37f5d559e99417d8fd14032def1238f16
Ivan <Ivan@host.docker.internal>
2023-04-10 08:56:06 +0200

    feat: apply damage to the item in each use

--- src/main/java/items/Grinder.java
+++ src/main/java/items/Grinder.java
@@ -17,7 +17,7 @@ import net.minecraft.world.level.Level;
 public class Grinder extends Item {
 
 	public Grinder() {
-		super(new Item.Properties().stacksTo(0).defaultDurability(100).tab(ItemInit.instance));
+		super(new Item.Properties().stacksTo(0).defaultDurability(150).tab(ItemInit.instance));
 
 	}
 
@@ -25,17 +25,24 @@ public class Grinder extends Item {
 	public InteractionResultHolder<ItemStack> use(Level level, Player entity, InteractionHand interactionHand) {
 		ItemStack budItemStack = new ItemStack(ItemInit.BUD.get());
 		ItemStack grindedWeedStack = new ItemStack(ItemInit.GRINDED.get());
+		ItemStack grinder = entity.getItemInHand(interactionHand);
 		Inventory inventory = entity.getInventory();
 
 		if (inventory.contains(budItemStack)) {
-
+			
 			inventory.removeItem(inventory.findSlotMatchingItem(budItemStack), 1);
 			if (inventory.getFreeSlot() < 0) {
+				
 				entity.drop(grindedWeedStack, false);
 			}
+			grinder.hurtAndBreak(2, entity, (Player) -> {
+				Player.broadcastBreakEvent(interactionHand);
+			
+			});
 			inventory.add(grindedWeedStack);
 			level.playLocalSound(entity.getX(), entity.getY(), entity.getZ(), SoundEvents.SPYGLASS_USE,
 					SoundSource.PLAYERS, 10, 1.0F, true);
+			
 			return InteractionResultHolder.success(entity.getItemInHand(interactionHand));
 		} else {
 
